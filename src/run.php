ef44c91bfff8b776dbf5a03996e711440a2fe1ab
dyh <yzcczdyh@gmail.com>
2017-12-08 17:29:27 +0800

    获取前200名

--- src/run.php
+++ src/run.php
@@ -9,9 +9,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use League\Csv\Writer;
-use \GuzzleHttp\Client;
+use GuzzleHttp\Client;
 
-function getRankList(Client $client, $date, $token)
+function getRankList(Client $client, $date, $token, $rankRange = 1)
 {
     $r = $client->request('POST', 'http://fsight.qq.com/GameListAjax', [
         'headers'     => [
@@ -22,39 +22,48 @@ function getRankList(Client $client, $date, $token)
         'form_params' => [
             'listCat'   => '3',//1：免费榜 2：付费榜 3：畅销榜
             'listType'  => '0',//有点多， 0为总榜
-            'rankRange' => '1',//区间 1：1~30 2:31~100 3:201~600 。。。 最大为8:1201~1391
+            'rankRange' => $rankRange,//区间 1：1~30 2:31~200 3:201~600 。。。 最大为8:1201~1391
             'listDate'  => $date
         ]
     ]);
 
     $body = json_decode($r->getBody()->getContents(), true);
 
-    return $body;
+    return $body['ret']['ranks'];
 }
 
-$client = new Client(['cookies' => true]);
-$r      = $client->request('GET', 'http://fsight.qq.com/GameList?type=hotRank');
+//获取某一天，排行榜的前200名
+function getTop200(Client $client, $date, $token)
+{
+    $r1_30   = getRankList($client, $date, $token, 1);
+    $r31_200 = getRankList($client, $date, $token, 2);
+    return array_merge($r1_30, $r31_200);
+}
 
-//var_dump($r->getHeaders());
-
+//获取token（不用纠结这一段）
+$client       = new Client(['cookies' => true]);
+$r            = $client->request('GET', 'http://fsight.qq.com/GameList?type=hotRank');
 $headers      = $r->getHeaders();
 $wetest_token = $headers['Set-Cookie'][3];
 $wetest_token = explode('=', $wetest_token)[1];
 $wetest_token = explode(';', $wetest_token)[0];
 $wetest_token = urldecode($wetest_token);
 
-$app = [];
-
 //起止时间
 $curDate = "2017-11-01";
 $endDate = "2017-12-01";
 
+//创建csv文件
 $rankCsv = Writer::createFromPath("data/{$curDate}_{$endDate}_rank.csv", 'w');
 $appCsv  = Writer::createFromPath("data/{$curDate}_{$endDate}_app.csv", 'w');
 
+//保存所有应用信息
+$app = [];
+
+//保存排名
 while ($curDate != $endDate) {
 
-    $ranks = getRankList($client, $curDate, $wetest_token)['ret']['ranks'];
+    $ranks = getTop200($client, $curDate, $wetest_token);
     $val   = [];
     foreach ($ranks as $rank) {
         $app[$rank['entityId']] = $rank['game_name'];
@@ -66,6 +75,7 @@ while ($curDate != $endDate) {
     $curDate = date('Y-m-d', strtotime($curDate . " +1 day"));
 }
 
+//保存应用信息
 foreach ($app as $key => $value) {
     $appCsv->insertOne([$key, $value]);
 }
